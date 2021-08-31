2bbdbeabb4f20331d27998e75d8e6d2bd5ec7947
Manfred Schulenburg <info@blueseal.eu>
2021-08-31 11:27:15 +0200

    Delete include directory

--- include/index_gamelink.php
+++
@@ -1,6 +0,0 @@
-<br><br>
-
-<p  style="text-align:center;">You know to write the basic alphabet with ten fingers? How about a little game?</p>
-
-<p  style="text-align:center; font-size: 24px;"><button title="bigger"><a href="typinggame/" target="_blank" style="text-decoration: none; color: inherit;"><span style="font-size: 18px">Speed typing game</a></button>
-
