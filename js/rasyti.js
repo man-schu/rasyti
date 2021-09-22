3d2aa50c34e63d25d32f8d307ad2993bdc066af5
Manfred Schulenburg <info@blueseal.eu>
2021-09-22 23:58:09 +0200

    Update rasyti.js

--- js/rasyti.js
+++ js/rasyti.js
@@ -626,7 +626,7 @@ var gLessons = (function(window, document, undefined) {
     // select a random line in the current level
    //var lines = lessonsDoc.getElementsByTagName('Level').item(index)
      //                     .getElementsByTagName('Line');
-	  var lines = lessonsDoc.getElementsByTagName('Line');
+	//  var lines = lessonsDoc.getElementsByTagName('Line');
     var i = Math.floor(Math.random() * lines.length);
     return lines[i].childNodes[0].nodeValue;
   }
