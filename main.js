2ea8a75deb7dff3a3e919a76f71b658c4ce77835
Manfred Schulenburg <info@blueseal.eu>
2021-09-28 21:33:24 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,3 @@
 webPreferences: {
    devTools: false
 }
-
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js")
-      // (NOT RECOMMENDED)
-      // If true, we can skip attaching functions from ./menu-functions.js to window object in preload.js.
-      // And, instead, we can use electron APIs directly in renderer.js
-      // From Electron v5, nodeIntegration is set to false by default. And it is recommended to use preload.js to get access to only required Node.js apis.
-      // nodeIntegration: true
-    },
-    frame: false //Remove frame to hide default menu
-  });
-  // ...other stuff
-}
-
-// Register an event listener. When ipcRenderer sends mouse click co-ordinates, show menu at that position.
-ipcMain.on(`display-app-menu`, function(e, args) {
-  if (isWindows && mainWindow) {
-    menu.popup({
-      window: mainWindow,
-      x: args.x,
-      y: args.y
-    });
-  }
-});
